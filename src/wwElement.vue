e6429fb066856bbf1da992cf9cee4e211520b129
Damien Duro <damien@weweb.io>
2023-01-27 17:23:57 +0100

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -30,14 +30,15 @@ export default {
     },
     emits: ['update:content:effect', 'trigger-event'],
     setup(props) {
-        let val = parseInt(props.content.value);
-        if (isNaN(val)) val = 0;
-
         const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable({
             uid: props.uid,
             name: 'value',
             type: 'number',
-            defaultValue: computed(() => val === undefined ? 0 : val),
+            defaultValue: computed(() => {
+                let val = parseInt(props.content.value);
+                if (isNaN(val)) val = 0;
+                return val === undefined ? 0 : val
+            }),
         });
         return { variableValue, setValue };
     },
